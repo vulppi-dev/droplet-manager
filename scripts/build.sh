285e02dfd7c82228310e49fa20adf74ef61a8f26
Renato Rodrigues <morbden.dev@gmail.com>
2022-06-22 15:05:50 -0300

    fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,13 +2,22 @@
 
 cd $BUILD_PATH && git clone $HOST $NAME
 
-APP_PATH=`${BUILD_PATH}/${NAME}`
+APP_PATH=$BUILD_PATH/$NAME
 
 if [ -f `$APP_PATH/package.json` ]; then
   npm --prefix=$APP_PATH --production=false i
-  cd $APP_PATH && rollup -c $BUILD_PATH/rollup.config.js
-  cp $APP_PATH/dist/index.js $APPS_PATH/$NAME.mjs
-  pm2 start $APPS_PATH/$NAME.mjs --name $NAME --update-env
+  npm --prefix=$BUILD_PATH/$NAME run build
+
+  if [ -f $BUILD_PATH/$NAME/dist/index.js ]; then
+    rimraf $APPS_PATH/$NAME
+    mkdir -p $APPS_PATH/$NAME
+    cp -r $BUILD_PATH/$NAME/dist $APPS_PATH/$NAME/
+    cp $BUILD_PATH/$NAME/package.json $APPS_PATH/$NAME/
+    npm --prefix=$APPS_PATH/$NAME i
+  fi
+
+  pm2 delete $NAME
+  cd $APPS_PATH/$NAME && pm2 start --name $NAME --update-env npm -- start
   pm2 save
 fi
 
